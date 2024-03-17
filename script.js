fcfe9af1e5ac7f3ad01bf3b0ccc183b40e5b4c27
HyperOmerJ <omer.tapdiqov@gmail.com>
2024-03-17 23:55:28 +0400

    updated

--- script.js
+++
@@ -1,9 +0,0 @@
-var cursor = document.getElementById("follow");
-
-document.addEventListener("mousemove",function(e){
-  var x = e.clientX;
-  var y = e.clientY;
-  cursor.style.left = x + "px";
-  cursor.style.top = y + "px";
-});
-
